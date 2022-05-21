f29a47f18487ef663db1dbab7fe7325bb75834da
msureshm <sureshmurthygari@gmail.com>
2022-05-21 12:56:44 +0530

    Displaying add question form in add-question.html page

--- js/home.js
+++ js/home.js
@@ -1,14 +1,13 @@
 $(document).ready(function(){
 	$("#logout").click(function(){
+		localStorage.setItem('user_group', "")
 		window.location.href = "login.html"
 	});
 	var user_group = localStorage.getItem('user_group')
 	if(user_group == 'admin'){
-		
+
 	}
 	else {
 		window.location.href = "login.html"
 	}
 });
-
-
